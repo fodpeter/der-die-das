e96c712345488bee5fcd5dbfc8430527bd63e851
Peter <fodpeter@gmail.com>
2018-07-04 11:42:16 +0200

    add github ribbon

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./App.css";
 import Game from "./game";
+import GitHubForkRibbon from "react-github-fork-ribbon";
 
 const App = () => (
-  <div className="app">
-    <header className="app-header">
-      <h1 className="app-title">Welcome to Der Die Das</h1>
-    </header>
-    <div className="app-intro">
-      <Game />
+  <Fragment>
+    <GitHubForkRibbon
+      href="https://github.com/fodpeter/der-die-das"
+      target="_blank"
+      position="right"
+    >
+      Fork me on GitHub
+    </GitHubForkRibbon>
+    <div className="app">
+      <header className="app-header">
+        <h1 className="app-title">Welcome to Der Die Das</h1>
+      </header>
+      <div className="app-intro">
+        <Game />
+      </div>
     </div>
-  </div>
+  </Fragment>
 );
 
 export default App;
